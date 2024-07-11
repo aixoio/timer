064829bc40ec18312c2d6e2b72f3858de6305748
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-11 21:28:38 +1000

    Update index page with new timer component

---
+++ src/components/Timer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="border border-base-300 p-4 rounded-lg shadow-sm">
+        <h1 class="text-3xl">Timer</h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
