950d8c44d060cda945e87df967c806930ee408e8
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-12 15:44:55 +1000

    Update tailwind.config.mjs file.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
 	plugins: [
 		require('daisyui'),
 	],
+	theme: {
+		extend: {},
+	},
 	daisyui: {
-		themes: ['winter']
+		themes: ['winter', 'dim'],
+		darkTheme: 'dim',
 	},
 }
